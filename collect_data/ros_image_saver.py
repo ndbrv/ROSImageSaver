08dfbe941443439c9495585c1f2e9e5109248390
ndbrv <52050364+ndbrv@users.noreply.github.com>
2020-02-26 21:13:21 -0500

    Add files via upload

--- collect_data/ros_image_saver.py
+++ collect_data/ros_image_saver.py
@@ -5,7 +5,7 @@
 import rospy
 # ROS Image message
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import PoseWithCovariance
+from geometry_msgs.msg import PoseWithCovarianceStamped
 # ROS Image message -> OpenCV2 image converter
 from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
@@ -25,29 +25,32 @@ def image_callback(msg):
         print(e)
     else:
         # Save your OpenCV2 image as a jpeg and write timestamp to a csv
-        cv2.imwrite('/home/nikola/catkin_ws/src/images/scamera_image.jpeg', cv2_img)
-	with open('timestamp.csv','w',newline='') as csvfile:
-		spamwriter = csv.writer(csvfile, delimiter='', quotechar='|', 				quoting=csv.QUOTE_MINIMAL)
-	spamwriter.writerow([rospy.get_rostime()])
+        cv2.imwrite('/home/turtlebot/catkin_ws/src/collect_data/images/camera_image'+str(msg.header.stamp)+'.jpeg', cv2_img)
+	with open('/home/turtlebot/catkin_ws/src/collect_data/timestamp.csv','w') as csvfile:
+		spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', 				quoting=csv.QUOTE_MINIMAL)
+		spamwriter.writerow([msg.header.stamp])
+	rospy.sleep(1)
+	
 
 def pose_callback(msg):
 	# Write pose to a csv
 	print("Received pose!")
-	with open('pose.csv','w',newline='') as csvfile:
-		spamwriter2 = csv.writer(csvfile, delimiter='', quotechar='|', 				quoting=csv.QUOTE_MINIMAL)
-	spamwriter2.writerow([msg.pose])
+	with open('/home/turtlebot/catkin_ws/src/collect_data/pose.csv','w') as csvfile2:
+		spamwriter2 = csv.writer(csvfile2, delimiter=' ', quotechar='|', 				quoting=csv.QUOTE_MINIMAL)
+		spamwriter2.writerow([msg.pose.pose.position.x,msg.pose.pose.position.y,msg.pose.pose.position.z])
 
 
 
 def main():
     rospy.init_node('image_listener')
     # Define your image topic
-    image_topic = "/cameras/left_hand_camera/image"
+    image_topic = "/camera/rgb/image_raw"
     # Set up your subscriber and define its callback
     rospy.Subscriber(image_topic, Image, image_callback)
-    rospy.Subscriber("amcl_pose", PoseWithCovariance, pose_callback)
+    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, pose_callback)
     # Spin until ctrl + c
     rospy.spin()
+    
 
 if __name__ == '__main__':
     main()
